3a79a1e5bff6ea21fb0dd865063cd3e214ec10bd
Yu-Hsuan Huang <yuhsuan.huang.tw@gmail.com>
2020-07-05 22:45:30 -0400

    update Java Exception Example code

--- JavaBasicTutorial/src/pers/sample/tutorial/exception/ExceptionExample.java
+++ JavaBasicTutorial/src/pers/sample/tutorial/exception/ExceptionExample.java
@@ -2,7 +2,9 @@
  * @author Yu-Hsuan Huang
  * 
  * How to use Exception structure
+ * 
  * try ... catch ... finally
+ * throws exception
  * 
  */
 package pers.sample.tutorial.exception;
@@ -11,13 +13,28 @@ public class ExceptionExample {
 	
 	public static void main(String[] args) {
 		
-		// NumberFormatException
+
+//		String str = "123"; //Correct Test
+		String str = "123a"; //Incorrect Test
+		
+		processExceptionInTryCatch(str);
+		
+		//If method may throw exception, you should process in where you invoke the method.
+		//Otherwise, this exception (error) will cause that shut down your application.
+		dontProcessException(str);
+	}
+	
+	//It will process exception in try-catch and print error/warning in Log file
+	public static void processExceptionInTryCatch(String str) {
+		
 		try {
-			String str = "123"; //Correct Test
-//			String str = "123a"; //Incorrect Test
 			Long tmp = Long.valueOf(str);
 			System.out.println(tmp);
-		} catch(NumberFormatException e) {
+		} catch(NumberFormatException e) { // NumberFormatException
+			System.out.println(e);
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} catch(Exception e) { // The parent class of all the exception class
 			System.out.println(e);
 			System.out.println(e.getMessage());
 			e.printStackTrace();
@@ -26,6 +43,12 @@ public class ExceptionExample {
 			System.out.println("end");
 		}
 		
+	}
+	
+	//It will not process exception. If exception occurs, it will throws the exception to the caller.
+	public static void dontProcessException(String str) throws NumberFormatException {
+		Long tmp = Long.valueOf(str);
+		System.out.println(tmp);
 	}
 	
 }
